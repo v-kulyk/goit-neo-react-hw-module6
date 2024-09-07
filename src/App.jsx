525fe062b8c937d9c19235516af313beec74ccab
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-09-07 11:14:18 +0300

    implement homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
-import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+import styles from "./App.module.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <div className={styles.app}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
